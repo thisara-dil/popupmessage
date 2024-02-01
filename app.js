6c62c746c6b025bb5ece9541b94e9efb196d88d8
S.A.T.D. Perara <136673697+thisara-dil@users.noreply.github.com>
2024-02-01 23:54:37 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+const showPopup = document.querySelector('.show-popup');
+const popupcontainer = document.querySelector('.popup-container');
+
+showPopup.onclick = () =>{
+    popupcontainer.classList.add('active')
+}
