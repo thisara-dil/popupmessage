fc369f95f4925f1d723905b382dd4854ba155179
S.A.T.D. Perara <136673697+thisara-dil@users.noreply.github.com>
2024-02-20 22:34:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,1 @@
-const showPopup = document.querySelector('.show-popup');
-const popupcontainer = document.querySelector('.popup-container');
 
-showPopup.onclick = () =>{
-    popupcontainer.classList.add('active')
-}
